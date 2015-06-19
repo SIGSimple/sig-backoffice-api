b4c2fdac8b4248019ee3161c6797011914e5ea28
Filipe Coelho <filipe.mendonca.coelho@gmail.com>
2015-06-19 13:27:44 -0300

    apontando pra minha maquina de novo

--- config/constants.php
+++ config/constants.php
@@ -5,10 +5,10 @@ define('CHARSET', 'utf8');
 // Constantes de configuração do banco de dados
 if(serverName() == 'localhost') {
 	// DESENVOLVIMENTO - LOCALHOST
-	define('HOST', 		'srvwldb1.cjnvfjneksls.sa-east-1.rds.amazonaws.com');
+	define('HOST', 		'localhost');
 	define('DBNAME', 	'bd_sig_backoffice');
 	define('USER', 		'root');
-	define('PASSWORD', 	'root1234567');
+	define('PASSWORD', 	'150679');
 }
 else
 {
