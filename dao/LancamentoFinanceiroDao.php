5e8ec8c149428115c6961b2af1643c01dfb092e1
Filipe Coelho <filipe.mendonca.coelho@gmail.com>
2015-12-21 11:10:43 -0200

    bugfix

--- dao/LancamentoFinanceiroDao.php
+++ dao/LancamentoFinanceiroDao.php
@@ -389,8 +389,21 @@ class LancamentoFinanceiroDao{
 		$sql = "CALL sp_consolidado_despesa_nat_op('$dta_inicio', '$dta_final');";
 		$select = $this->conn->prepare($sql);
 		if($select->execute()){
-			if($select->rowCount()>0)
-				return $select->fetchALL(PDO::FETCH_ASSOC);
+			if($select->rowCount()>0) {
+				$data = $select->fetchALL(PDO::FETCH_ASSOC);
+
+				$newData = [];
+				foreach ($data as $key => $value) {
+					$newData[] = array(
+						'Cód. Natureza de Operação' => $value['num_item'], 
+						'Natureza de Operação' => $value['dsc_item'],
+						'Origem da Despesa' => $value['dsc_origem'],
+						'Valor' => $value['vlr_lancamento']
+					);
+				}
+
+				return $newData;
+			}
 			else
 				return false;
 		}
